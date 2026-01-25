c442ede848eb28f3958063e048262b1fdb757257
edgahhhh <ehernandez7419@gmail.com>
2026-01-25 15:51:49 -0700

    started avl analysis module in py_planes that can be useful in storing data from an avl stability file

---
+++ tools/aero_techt_py/py_planes/avl_analysis.py
@@ -0,0 +1,8 @@
+""" avl analysis """
+from py_planes.avl import Avl, Aircraft
+
+class AvlAnalysis():
+    """ AVL analysis class """
+    def __init__(self, avl:Avl):
+        """ create AvlAnalysis object with associated st and sb file """
+        return None
